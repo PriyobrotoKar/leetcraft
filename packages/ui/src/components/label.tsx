92c5f9626aa7a6faa3d209d6000fe6c2d7bc4351
Priyobroto Kar <priyobrotokar@gmail.com>
2025-05-12 21:38:35 +0530

    feat(ui): add new package of UI for centralizing ShadcnUI components

---
+++ packages/ui/src/components/label.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import * as LabelPrimitive from '@radix-ui/react-label';
+
+import { cn } from '@leetcraft/ui/lib/utils';
+
+function Label({
+  className,
+  ...props
+}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+  return (
+    <LabelPrimitive.Root
+      data-slot="label"
+      className={cn(
+        'text-md flex select-none items-center gap-2 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Label };
