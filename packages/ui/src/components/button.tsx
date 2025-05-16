0b3a02961dee7a337bbd15eed488fe7352bdc4e3
Priyobroto Kar <priyobrotokar@gmail.com>
2025-05-16 13:45:43 +0530

    feat(ui): add table component

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -27,7 +27,7 @@ const buttonVariants = cva(
       size: {
         default:
           'h-11 px-6 py-3 has-[>svg]:px-3 inset-shadow-xs border-b-1 ring-1',
-        sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
+        sm: 'h-10 text-md-medium rounded-md gap-1.5 px-3 inset-shadow-xs border-b-1 ring-1 has-[>svg]:px-3',
         lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
         icon: 'size-9',
       },
